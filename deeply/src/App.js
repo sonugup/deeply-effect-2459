e2d59961c3ce9ca015d8334a4a46a3d0a585347c
Sonu Gupta <rg3029926@gmail.com>
2022-09-28 16:35:14 +0530

    day-1

---
+++ deeply/src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Home from './Home/Home';
+
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ deeply/src/Components/Navbar.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <>
+            {/* <h1>I am Navbar </h1> */}
+            <div className="Nav">
+                <div className="Nav2">
+                    {/* <div></div> */}
+                    <div className="Nav-1">
+                        <div className="Nav-2">
+                            <span>Tool</span>
+
+                            <div className="Nav-3">
+                                <div className="In">
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="Nav-2">
+                            <span>Explore</span>
+
+                            <div className="Nav-3">
+                                <div className="In">
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                    <div>
+                                        <h3>Publishing</h3>
+                                        <p>Plan, collaborate, and publish thumb-stopping content</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="Nav-2">
+                            <h3>Pricing</h3>
+                        </div>
+                        <div className="Nav-2">
+                            <h3>Blog</h3>
+                        </div>
+                        <div className="Nav-2">
+                            <h3>About</h3>
+                        </div>
+                        <div className="Nav-2">
+                            <h3>Customers</h3>
+                        </div>
+                        <div className="Nav-2">
+                            <h3><a href="#">Login</a></h3>
+                        </div>
+                        <div className="Nav-2">
+                            <h3><button>Get starte now</button></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Navbar;
